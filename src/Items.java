9e93fd9dc904896abf9e838e08c6d9be1d90608f
midotronn <mohassan1137@gmail.com>
2021-10-30 13:10:45 -0500

    completed items

--- src/Items.java
+++ src/Items.java
@@ -1,4 +1,26 @@
-
 public class Items {
+
+	private String name;
+	private boolean available;
 	
+	// Makes the new item
+	Items(String name, boolean available){
+		this.name =  name;
+		this.available = available;
+	}
+	
+	// getter method for the name
+	public String getName() {
+		return this.name;
+	}
+	
+	// getter method for the availability
+	public boolean getAvailabee() {
+		return this.available;
+	}
+	
+	// sets the availability
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
 }
