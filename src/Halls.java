dc6566b4465ad9f099daa2ff2d534e75411abb87
Joshua Clark <Joshrclark02@hotmail.com>
2021-10-30 13:03:43 -0500

    Halls Setup

--- src/Halls.java
+++ src/Halls.java
@@ -1,4 +1,18 @@
 
 public class Halls {
-
+	private String name;
+	private Items[] menu;
+	
+	Halls(String name, Items[] menu){
+		this.name = name;
+		this.menu = menu;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public Items[] getMenu() {
+		return this.menu;
+	}
 }
