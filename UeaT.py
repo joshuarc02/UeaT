1418922486fb633019f5d9e7d38eab9986dd4d1b
Joshua Clark <Joshrclark02@hotmail.com>
2021-10-31 10:25:50 -0500

    So much

--- UeaT.py
+++ UeaT.py
@@ -1,4 +1,6 @@
 class Hall:
+    names = []
+
     def __init__(self, name, fileName):
         self.name = name
 
@@ -6,12 +8,26 @@ class Hall:
 
         self.items = []
         for line in file:
-            name, number = line.rsplit(" ", 1)
+            name, number = line.strip().rsplit(" ", 1)
+            if name in Hall.names:
+                continue
+            Hall.names.append(name)
             item = Item(name, number)
             self.items.append(item)
+
+        self.items = sorted(self.items, key=lambda item: item.name.lower())
 
 
 class Item:
     def __init__(self, name, number):
-        self.name = name
+        self.name = name.replace(' ', '_')
         self.number = number
+
+    def getAvaliable(self):
+        if self.number == '1':
+            return '✓'
+        else:
+            return 'X'
+
+    def toString(self):
+        return item.name + " " + number
--- app/routes.py
+++ app/routes.py
@@ -10,8 +10,8 @@ from UeaT import *
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
     # initalizing all the vars for the page
-    session['J2_JCL'] = Hall('J2_JCL', 'J2_JCL_menu.txt')
-    session['Kins'] = Hall('Kins', 'Kins_menu.txt')
+    session['J2_and_JCL_Dining'] = Hall('J2_and_JCL_Dining', 'J2_and_JCL_Dining_menu.txt')
+    session['Kinsolving_Dining'] = Hall('Kinsolving_Dining', 'Kinsolving_Dining_menu.txt')
 
     # renders the given template and then defines vars
     return render_template('index.html', title='Home')
@@ -19,8 +19,25 @@ def index():
 
 @app.route('/generic', methods = ['GET', 'POST'])
 def generic():
+    name = request.args.get('hall')
+    menu = session[name].items
+    if request.method == 'POST':
+        f = open("demofile2.txt", "a")
+        for item in menu:
+            avaliable = request.form[item.name]
+            if avaliable:
+                item.number = '1'
+            else:
+                item.number = '0'
+            
+            f.write(item.toString())
+        f.close()
+        return redirect(url_for('generic'))
+
+
+    name = name.replace('_', ' ')
     # renders the given template and then defines vars
-    return render_template('generic.html', title='Home')
+    return render_template('generic.html', title='Home', name=name, menu=menu)
 
 @app.route('/elements', methods = ['GET', 'POST'])
 def elements():
