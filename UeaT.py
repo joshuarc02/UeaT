8831fb6fa58f2ead84f427ae1d62fe8bae81ebf1
Joshua Clark <Joshrclark02@hotmail.com>
2021-11-02 19:24:49 -0500

    Added functionality for grabbing the daily menu

--- UeaT.py
+++ UeaT.py
@@ -17,6 +17,32 @@ class Hall:
 
         self.items = sorted(self.items, key=lambda item: item.name.lower())
 
+    def update_menus():
+        J2L_URL = "http://hf-food.austin.utexas.edu/foodpro/shortmenu.aspx?sName=University+Housing+and+Dining&locationNum=12&locationName=Jester+Dining%3a+J2+%26+JCL&naFlag=1"
+        J2L_menu = Hall.get_menu(J2L_URL)
+        Hall.save_menu(J2L_menu, "J2_and_JCL_Dining")
+
+        KIN_URL = "http://hf-food.austin.utexas.edu/foodpro/shortmenu.aspx?sName=University+Housing+and+Dining&locationNum=03&locationName=Kins+Dining&naFlag=1"
+        KIN_menu = Hall.get_menu(KIN_URL)
+        Hall.save_menu(J2L_menu, "Kinsolving_Dining")
+    
+    def get_menu(URL):
+        import requests
+        from bs4 import BeautifulSoup
+
+        page = requests.get(URL)
+        page = BeautifulSoup(page.content, "html.parser")
+
+        menu_items = page.find_all("div", class_="shortmenurecipes")
+        menu_items = [item.find("span").text.strip() for item in set(menu_items)]
+
+        return menu_items
+
+    def save_menu(menu_items, name):
+            f = open(name + "_menu.txt", "w")
+            for item in menu_items:
+                f.write(item + " 1\n")
+            f.close()
 
 class Item:
     def __init__(self, name, number):
@@ -31,3 +57,5 @@ class Item:
 
     def toString(self):
         return self.name + " " + self.number
+
+Hall.update_menus()
